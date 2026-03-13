dc71fc2ab56a1caf7f8107e9dd24cddc6486594a
bmartinez887 <bmartinez887@uvaq.edu.mx>
2026-03-12 19:01:34 -0600

    message interaction

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading;
 
@@ -9,6 +9,7 @@ class ChatServer
 {
     static TcpListener? server;
     static List<TcpClient> clients = new List<TcpClient>();
+    static Dictionary<TcpClient, string> clientNames = new Dictionary<TcpClient, string>();
 
     static void Main()
     {
@@ -26,30 +27,71 @@ class ChatServer
             t.Start(client);
         }
     }
-    
+
     static void HandleClient(object obj)
     {
         TcpClient client = (TcpClient)obj;
         NetworkStream stream = client.GetStream();
         byte[] buffer = new byte[1024];
-        //int bytesRead;
 
-        while (true)
+        try
         {
+            // First message received will be the user's name
             int bytesRead = stream.Read(buffer, 0, buffer.Length);
-            string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-            Console.WriteLine("Received message: " + message);
-            Broadcast(message);
+            if (bytesRead == 0) return;
 
+            string userName = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            clientNames[client] = userName;
+
+            Console.WriteLine(userName + " joined the chat.");
+
+            while (true)
+            {
+                bytesRead = stream.Read(buffer, 0, buffer.Length);
+
+                if (bytesRead == 0)
+                {
+                    Console.WriteLine(userName + " disconnected.");
+                    break;
+                }
+
+                string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                string fullMessage = userName + ": " + message;
+
+                Console.WriteLine("Received message: " + fullMessage);
+                Broadcast(fullMessage);
+            }
+        }
+        catch
+        {
+            if (clientNames.ContainsKey(client))
+            {
+                Console.WriteLine(clientNames[client] + " disconnected unexpectedly.");
+            }
+        }
+        finally
+        {
+            clients.Remove(client);
+            clientNames.Remove(client);
+            client.Close();
         }
     }
+
     static void Broadcast(string message)
     {
         byte[] data = Encoding.UTF8.GetBytes(message);
-        foreach(var client in clients)
+
+        foreach (var client in clients)
         {
-            NetworkStream stream = client.GetStream();
-            stream.Write(data, 0, data.Length);
+            try
+            {
+                NetworkStream stream = client.GetStream();
+                stream.Write(data, 0, data.Length);
+            }
+            catch
+            {
+                // Ignore errors for disconnected clients
+            }
         }
     }
 }
